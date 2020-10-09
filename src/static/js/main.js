24a79017460481db94f851affe9d37df0e957b6a
Robert Goldmann <deadlocker@gmx.de>
2020-10-10 01:21:28 +0200

    #3 - remove jquery leftovers

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,7 +1,7 @@
 let options = {
     cellHeight: '10vh',
     margin: '2vh',
-    column : 12,
+    column: 12,
     disableDrag: true,
     disableResize: true,
 };
@@ -11,15 +11,18 @@ let grid = GridStack.init(options);
 
 let url = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '');
 let socket = io.connect(url + '/update');
-socket.on('tileUpdate', function (msg) {
+socket.on('tileUpdate', function(msg)
+{
     let data = JSON.parse(msg);
     let node = document.querySelector('#tile-' + data["uniqueName"] + ' .container');
 
-    if (node === null) {
+    if(node === null)
+    {
         return;
     }
 
-    if (data["content"] === null) {
+    if(data["content"] === null)
+    {
         node.innerHTML = '<div class="placeholder">❌ Data not yet available</div>';
         return;
     }
@@ -29,17 +32,23 @@ socket.on('tileUpdate', function (msg) {
     // scripts have to be added dynamically instead of "innerHTML"
     // (otherwise they won't be executed due to browser code injection counter measurements)
     let scriptTags = node.querySelectorAll('script');
-    for (script of scriptTags) {
-        var newScript = document.createElement("script");
-        var inlineScript = document.createTextNode(script.innerText);
+    for(script of scriptTags)
+    {
+        let newScript = document.createElement("script");
+        let inlineScript = document.createTextNode(script.innerText);
         newScript.appendChild(inlineScript);
         script.parentNode.appendChild(newScript);
         script.remove();
     }
 });
 
-$('.grid-stack-item').click(function () {
-    let tileName = this.id.replace('tile-', '');
-    console.log('Manual refresh for tile "' + tileName + '"');
-    socket.emit('refresh', tileName);
-});
+let items = document.querySelectorAll('.grid-stack-item');
+for(let i = 0; i < items.length; i++)
+{
+    items[i].addEventListener('click', function(event)
+    {
+        let tileName = this.id.replace('tile-', '');
+        console.log('Manual refresh for tile "' + tileName + '"');
+        socket.emit('refresh', tileName);
+    });
+}
