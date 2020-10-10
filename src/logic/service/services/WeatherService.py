4e0d0f18d77f052d3a251c80a5620554b9a26a2c
Robert Goldmann <deadlocker@gmx.de>
2020-10-10 16:50:32 +0200

    WeatherService: added example settings

--- src/logic/service/services/WeatherService.py
+++ src/logic/service/services/WeatherService.py
@@ -8,6 +8,16 @@ from TheCodeLabs_BaseUtils.MultiCacheKeyService import MultiCacheKeyService
 class WeatherService(MultiCacheKeyService):
     URL = 'https://api.openweathermap.org/data/2.5/onecall'
 
+    """
+    Fetches weather forecast information from OpenWeatherMap.
+    """
+
+    EXAMPLE_SETTINGS = {
+        "lat": "51.012825",
+        "lon": "13.666365",
+        "apiKey": "myApiKey"
+    }
+
     def _fetch_data(self, settings: Dict) -> Dict:
         response = requests.get(self.URL, params={
             'lat': settings['lat'],
