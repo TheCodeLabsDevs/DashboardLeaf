182ff441c610ea2dabde139112c4b4223751dc6d
Robert Goldmann <deadlocker@gmx.de>
2020-10-11 11:10:00 +0200

    codingstyle

--- src/logic/Helpers.py
+++ src/logic/Helpers.py
@@ -2,5 +2,5 @@ def join_url_parts(*args: str) -> str:
     return '/'.join(part.strip('/') for part in args)
 
 
-def round_to_decimals(value, decimals):
+def round_to_decimals(value, decimals) -> str:
     return '{:.{}f}'.format(value, decimals)
