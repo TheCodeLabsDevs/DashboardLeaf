7c0d2d5947ac97841d98619eed1c048432ec22db
Robert Goldmann <deadlocker@gmx.de>
2020-10-11 20:58:31 +0200

    Fixed #6 - Tile: weather forecast 7 days

---
+++ src/logic/tile/tiles/SevenDaysForecastTile.py
@@ -0,0 +1,53 @@
+import os
+import uuid
+from datetime import datetime
+from typing import Dict
+
+from flask import Blueprint
+
+from logic import Helpers
+from logic.service.ServiceManager import ServiceManager
+from logic.tile.Tile import Tile
+
+
+class SevenDaysForecastTile(Tile):
+    DATE_FORMAT = '%Y-%m-%d'
+
+    def __init__(self, uniqueName: str, settings: Dict, intervalInSeconds: int):
+        super().__init__(uniqueName, settings, intervalInSeconds)
+
+    def fetch(self, pageName: str) -> Dict:
+        weatherService = ServiceManager.get_instance().get_service_by_type_name('WeatherService')
+        cacheKey = f'{pageName}_{self._uniqueName}'
+
+        fetchIntervalInSeconds = 60 * 10  # query api less often
+        weatherData = weatherService.get_data(cacheKey, fetchIntervalInSeconds, self._settings)
+
+        forecast = weatherData['daily']
+
+        forecastData = {}
+        for day in forecast:
+            date = day['dt']
+            date = datetime.fromtimestamp(date)
+            date = datetime.strftime(date, self.DATE_FORMAT)
+            forecastData[date] = (int(day['temp']['min']), int(day['temp']['max']))
+
+        formattedDates = [f'<b>{value}</b>' for value in forecastData.keys()]
+        minValues = [x[0] for x in forecastData.values()]
+        maxValues = [x[1] for x in forecastData.values()]
+
+        return {
+            'formattedDates': list(forecastData.keys()),
+            'minValues': minValues,
+            'maxValues': maxValues
+        }
+
+    def render(self, data: Dict) -> str:
+        return Tile.render_template(os.path.dirname(__file__), __class__.__name__,
+                                    formattedDates=data['formattedDates'],
+                                    minValues=data['minValues'],
+                                    maxValues=data['maxValues'],
+                                    chartId=str(uuid.uuid4()))
+
+    def construct_blueprint(self, pageName: str, *args, **kwargs):
+        return Blueprint(f'{pageName}_{__class__.__name__}_{self.get_uniqueName()}', __name__)
