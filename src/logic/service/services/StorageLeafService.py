3f991ec2e74f8e6261d20a3f681abcd21318ae92
Robert Goldmann <deadlocker@gmx.de>
2020-10-14 21:22:14 +0200

    StorageLeafService: adapt to new date range parameters (instead of limit)

--- src/logic/service/services/StorageLeafService.py
+++ src/logic/service/services/StorageLeafService.py
@@ -1,3 +1,4 @@
+import urllib.parse
 from typing import Dict
 
 import requests
@@ -14,21 +15,38 @@ class StorageLeafService(MultiCacheKeyService):
     EXAMPLE_SETTINGS = {
         "url": "http://127.0.0.1:10003",
         "sensorID": 1,
-        "fetchType": "latest" or "all",
-        "fetchLimit": 100
-    }
-
-    URL_BY_FETCH_TYPE = {
-        'latest': 'measurements/latest',
-        'all': 'measurements/'
+        "sensorIDsForMinMax": [2, 3, 4],
+        "fetchType": "latest" or "all" or "minMax",
+        "startDateTime": "2020-01-30 18:03:15",
+        "endDateTime": "2020-01-30 20:03:15"
     }
 
     def _fetch_data(self, settings: Dict) -> Dict:
-        urlSensorInfo = Helpers.join_url_parts(settings['url'], 'sensor', str(settings['sensorID']))
-
         fetchType = settings['fetchType']
+        if fetchType == 'minMax':
+            joinedIDs = ','.join([str(item) for item in settings["sensorIDsForMinMax"]])
+            startDateTime = urllib.parse.quote(settings['startDateTime'])
+            endDateTime = urllib.parse.quote(settings['endDateTime'])
+
+            urlMinMax = Helpers.join_url_parts(settings['url'],
+                                               f'measurements/minMax?sensorIds={joinedIDs}'
+                                               f'&startDateTime={startDateTime}'
+                                               f'&endDateTime={endDateTime}')
+            minMaxResponse = requests.get(urlMinMax)
+            if minMaxResponse.status_code != 200:
+                raise Exception(f'Invalid status code: {minMaxResponse.status_code}')
+            return minMaxResponse.json()
+
+        urlSensorInfo = Helpers.join_url_parts(settings['url'], 'sensor', str(settings['sensorID']))
         if fetchType == 'all':
-            urlSensorValue = Helpers.join_url_parts(urlSensorInfo, 'measurements', str(settings['fetchLimit']))
+            if 'startDateTime' in settings and 'endDateTime' in settings:
+                startDateTime = urllib.parse.quote(settings['startDateTime'])
+                endDateTime = urllib.parse.quote(settings['endDateTime'])
+                urlSensorValue = Helpers.join_url_parts(urlSensorInfo,
+                                                        f'measurements'
+                                                        f'?startDateTime={startDateTime}&endDateTime={endDateTime}')
+            else:
+                urlSensorValue = Helpers.join_url_parts(urlSensorInfo, 'measurements')
         else:
             urlSensorValue = Helpers.join_url_parts(urlSensorInfo, 'measurements/latest')
 
