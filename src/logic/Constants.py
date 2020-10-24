c921f74747f46c20868efd577d515fab7a33097d
Robert Goldmann <deadlocker@gmx.de>
2020-10-24 19:21:25 +0200

    extracted colors to constants

--- src/logic/Constants.py
+++ src/logic/Constants.py
@@ -1,4 +1,13 @@
 import os
 
+from TheCodeLabs_BaseUtils.Color import Color
+
 APP_NAME = 'DashboardLeaf'
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+# colors
+RED = Color(230, 76, 60)
+ORANGE = Color(254, 151, 0)
+GREEN = Color(117, 190, 84)
+BLUE = Color(70, 138, 221)
+WHITE = Color(255, 255, 255)
--- src/logic/Helpers.py
+++ src/logic/Helpers.py
@@ -1,3 +1,6 @@
+from logic import Constants
+
+
 def join_url_parts(*args: str) -> str:
     return '/'.join(part.strip('/') for part in args)
 
@@ -8,36 +11,36 @@ def round_to_decimals(value, decimals) -> str:
 
 def determine_color_for_temperature(temperature: float) -> str:
     if temperature < 0:
-        return 'rgba(70, 138, 221, 1)'  # blue
+        return Constants.BLUE.to_rgba()
     elif temperature < 20:
-        return 'rgba(117, 190, 84, 1)'  # green
+        return Constants.GREEN.to_rgba()
     elif temperature < 25:
-        return 'rgba(254, 151, 0, 1)'  # orange
+        return Constants.ORANGE.to_rgba()
     else:
-        return 'rgba(230, 76, 60, 1)'  # red
+        return Constants.RED.to_rgba()
 
 
 def determine_color_for_wind(windSpeed: float) -> str:
     if windSpeed < 20:
-        return 'rgba(255, 255, 255, 1)'  # white
+        return Constants.WHITE.to_rgba()
     elif windSpeed < 60:
-        return 'rgba(254, 151, 0, 1)'  # orange
+        return Constants.ORANGE.to_rgba()
     else:
-        return 'rgba(230, 76, 60, 1)'  # red
+        return Constants.RED.to_rgba()
 
 
 def determine_color_for_weather_icon(iconId: int):
     if 200 <= iconId < 300:  # thunderstorm
-        return 'rgba(230, 76, 60, 1)'  # red
+        return Constants.RED.to_rgba()
     elif 300 <= iconId < 400:  # drizzle
-        return 'rgba(70, 138, 221, 1)'  # blue
+        return Constants.BLUE.to_rgba()
     elif 500 <= iconId < 600:  # rain
-        return 'rgba(70, 138, 221, 1)'  # blue
+        return Constants.BLUE.to_rgba()
     elif 600 <= iconId < 700:  # snow
-        return 'rgba(255, 255, 255, 1)'  # white
+        return Constants.WHITE.to_rgba()
     elif 700 <= iconId < 800:  # fog
-        return 'rgba(255, 255, 255, 1)'  # white
+        return Constants.WHITE.to_rgba()
     elif 800 <= iconId < 802:  # clear of partly cloudy (up to 25%)
-        return 'rgba(254, 151, 0, 1)'  # orange
+        return Constants.ORANGE.to_rgba()
     elif 802 <= iconId < 805:  # clouds > 25%
-        return 'rgba(255, 255, 255, 1)'  # white
+        return Constants.WHITE.to_rgba()
