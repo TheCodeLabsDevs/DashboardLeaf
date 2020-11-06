b5e688b35a9ff8588ab1bbd4905752fddedbabd7
Robert Goldmann <deadlocker@gmx.de>
2020-11-06 22:30:22 +0100

    Fixed #8 - firefox grid position bug (sort tiles by x,y)

--- src/logic/page/PageInstance.py
+++ src/logic/page/PageInstance.py
@@ -14,3 +14,6 @@ class TileLayoutSettings:
 class PageInstance:
     uniqueName: str
     tileLayouts: Dict[str, TileLayoutSettings]
+
+    def get_tile_layouts_sorted(self):
+        return {k: v for k, v in sorted(self.tileLayouts.items(), key=lambda item: item)}
