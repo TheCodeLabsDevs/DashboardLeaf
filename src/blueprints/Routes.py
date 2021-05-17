17d5f4edcf6dabb7f67584105da4d8fab5600821
Robert Goldmann <deadlocker@gmx.de>
2021-05-17 21:32:03 +0200

    updated dependencies

--- src/blueprints/Routes.py
+++ src/blueprints/Routes.py
@@ -6,12 +6,12 @@ from logic.page import PageManager
 def construct_blueprint(settings, pageManager: PageManager):
     routes = Blueprint('routes', __name__)
 
-    @routes.route('/', methods=['GET'])
+    @routes.get('/')
     def index():
         pageNames = pageManager.get_all_available_page_names()
         return render_template('index.html', pageNames=pageNames)
 
-    @routes.route('/page/<pageName>', methods=['GET'])
+    @routes.get('/page/<pageName>')
     def show_page(pageName: str):
         pageInstance = pageManager.get_page_instance_by_name(pageName)
         return render_template('page.html', page=pageInstance)
